44440a3e172f88b6701b3920f189daabb8a3a16a
Tal <trimoni8@gmail.com>
2023-01-20 09:45:14 -0500

    score

--- quiz.py
+++ quiz.py
@@ -6,10 +6,12 @@ if playing.lower() != 'yes':
   quit()
 
 print("Okay! Let's Play ")
+score = 0
 
 answer = input("What does CPU stand for? ")
 if answer.lower() == 'central processing unit':
   print('Correct!')
+  score += 1
 else:
   print('Incorrect!')
 
