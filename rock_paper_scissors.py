813d39884a535c8faeafb845e7a6f40982522e94
Tal <trimoni8@gmail.com>
2023-01-25 10:57:11 -0500

    options

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -3,6 +3,8 @@ import random
 user_wins = 0
 computer_wins = 0
 
+options = ["rock", "paper", "scissors"]
+
 while True:
   user_input = input('Type Rock/Paper/Scissors or Q to Quit: ').lower()
   if user_input == 'q':
@@ -12,6 +14,7 @@ while True:
     continue
 
   random_number = random.randint(0, 2)
-  #
+  # rock: 0, paper: 1, scissors: 2
+
 
 print('Leave')
