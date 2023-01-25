e0b25b5f8b2a35005d1339bcb29b12f51b09b0ce
Tal <trimoni8@gmail.com>
2023-01-25 10:22:35 -0500

    if else

--- guessing.py
+++ guessing.py
@@ -5,5 +5,11 @@ top_of_range = input('Type a number: ')
 if top_of_range.isdigit():
   top_of_range = int(top_of_range)
 
+  if top_of_range <= 0:
+    print('Please type a number larger than 0 next time')
+    quit()
+  else:
+    print('Please type a number next time')
+
 randum_number = random.randrange(0, 11)
 
