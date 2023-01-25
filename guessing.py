9b80ffd274d617678ad198e84c6c2565d5e2f5bc
Tal <trimoni8@gmail.com>
2023-01-25 10:27:25 -0500

    rand numb

--- guessing.py
+++ guessing.py
@@ -2,14 +2,19 @@ import random
 
 top_of_range = input('Type a number: ')
 
+random_number = random.randint(0, top_of_range)
+
 while True:
   user_guess = input('Make a guess: ')
   if user_guess.isdigit():
     user_guess = int(user_guess)
   else:
     print('Please type a number next time')
-    quit()
+    continue
 
-randum_number = random.randint(0, top_of_range)
+  if user_guess == random_number:
+    print('You got it boi!')
+  else:
+    print('Nope')
 
 
