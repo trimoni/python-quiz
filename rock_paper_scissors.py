53a6bd46b943472b5d76778dc00446597dd1e00c
Tal <trimoni8@gmail.com>
2023-01-25 10:59:07 -0500

    computer picked

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -10,11 +10,13 @@ while True:
   if user_input == 'q':
     break
   
-  if user_input not in ["rock", "paper", "scissors"]:
+  if user_input not in options:
     continue
 
   random_number = random.randint(0, 2)
   # rock: 0, paper: 1, scissors: 2
+  computer_pick = options[random_number]
+  print('computer picked', computer_pick)
 
 
 print('Leave')
