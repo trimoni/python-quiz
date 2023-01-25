8c57ca1a40346e02ea774be78770ed60a98fa768
Tal <trimoni8@gmail.com>
2023-01-25 11:02:20 -0500

    elif

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -16,7 +16,23 @@ while True:
   random_number = random.randint(0, 2)
   # rock: 0, paper: 1, scissors: 2
   computer_pick = options[random_number]
-  print('computer picked', computer_pick)
+  print('computer picked', computer_pick + '.')
 
+  if user_input == "rock" and computer_pick == 'scissors':
+    print('You Won!')
+    user_wins += 1
+    continue
+
+  elif user_input == "paper" and computer_pick == 'rock':
+    print('You Won!')
+    user_wins += 1
+
+  elif user_input == "scissors" and computer_pick == 'paper':
+    print('You Won!')
+    user_wins += 1
+
+  else:
+    print('You Lost, boo-hoo')
+    computer_wins += 1
 
 print('Leave')
