2b131326c12c811952b8ca93e478b02064978e17
Tal <trimoni8@gmail.com>
2023-01-25 10:36:49 -0500

    save

--- guessing.py
+++ guessing.py
@@ -16,6 +16,7 @@ random_number = random.randint(0, top_of_range)
 guesses = 0
 
 while True:
+  guesses += 1
   user_guess = input('Make a guess: ')
   if user_guess.isdigit():
     user_guess = int(user_guess)
@@ -27,5 +28,10 @@ while True:
     print('You got it boi!')
     break
   else:
-    print('Nope')
+    if user_guess > random_number:
+      print('Above number')
+    else:
+      print('Below number')
 
+
+print("You got it in", guesses, 'guesses')
