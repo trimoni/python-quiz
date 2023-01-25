7bd26496839af87a116c6d200695aa878cae237d
Tal <trimoni8@gmail.com>
2023-01-25 10:25:48 -0500

    while

--- guessing.py
+++ guessing.py
@@ -2,19 +2,14 @@ import random
 
 top_of_range = input('Type a number: ')
 
-if top_of_range.isdigit():
-  top_of_range = int(top_of_range)
-
-  if top_of_range <= 0:
-    print('Please type a number larger than 0 next time')
+while True:
+  user_guess = input('Make a guess: ')
+  if user_guess.isdigit():
+    user_guess = int(user_guess)
+  else:
+    print('Please type a number next time')
     quit()
-else:
-  print('Please type a number next time')
-  quit()
 
 randum_number = random.randint(0, top_of_range)
 
-while True:
-  user_guess = input('Make a guess: ')
-  break
 
