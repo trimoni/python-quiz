70c024286a318168e4582cc3a60fb1249ca8c80d
Tal <trimoni8@gmail.com>
2023-01-26 09:00:40 -0500

    if else 2

--- adventure.py
+++ adventure.py
@@ -4,7 +4,16 @@ print('Welcome', name, 'to this adventure!')
 answer = input("You are on a dirt road, it has come to an end and you can go left or right. Which way would you ike to go? ").lower()
 
 if answer == 'left':
-  print()
+  answer = input('You come to a river, you can walk around it or swim across? Type to walk around and swim to swim across: ')
+
+  if answer == 'swim':
+    print()
+  elif answer == 'walk':
+    print()
+  else:
+    print('Not a valid option. You suck.')
+
+
 elif answer == 'right':
   print()
 else:
