5be13e1597dece9af6b43aa11a696cb9f27042f8
Tal <trimoni8@gmail.com>
2023-01-26 08:57:08 -0500

    input

--- adventure.py
+++ adventure.py
@@ -1,2 +1,5 @@
 name = input('Type your name: ')
 print('Welcome', name, 'to this adventure!')
+
+answer = input("You are on a dirt road, it has come to an end and you can go left or right. Which way would you ike to go?")
+
