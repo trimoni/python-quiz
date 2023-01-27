e570222c68c8a6d7f6b4e203e514097be96c5613
Tal <trimoni8@gmail.com>
2023-01-27 10:48:47 -0500

    split

--- password.py
+++ password.py
@@ -3,7 +3,8 @@ master_pwd = input('What is the master password? ')
 def view():
   with open('passwords.txt', 'r') as f:
     for line in f.readlines():
-      print(line.rstrip())
+      data = line.rstrip()
+      user, passw = data.split('|')
 
 
 
