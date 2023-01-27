d29d13c09572107d6d568c8c503aac2533e3c584
Tal <trimoni8@gmail.com>
2023-01-27 10:39:12 -0500

    while

--- password.py
+++ password.py
@@ -1,9 +1,15 @@
 pwd = input('What is the master password? ')
 
-mode = input('Would you like to add a new password or view existing ones (view, add)? ')
-if mode == 'view':
-  pass
-elif mode == 'add':
-  pass
-else:
-  print('Invalid mode.')
+
+while True:
+  mode = input('Would you like to add a new password or view existing ones (view, add)?, press q to quit ').lower()
+  if mode == 'q':
+    break
+
+  if mode == 'view':
+    pass
+  elif mode == 'add':
+    pass
+  else:
+    print('Invalid mode.')
+    continue
