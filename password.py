edcab83915b2f32b5223882a6a7acac55536ab25
Tal <trimoni8@gmail.com>
2023-01-27 11:05:54 -0500

    erase str

--- password.py
+++ password.py
@@ -22,14 +22,16 @@ def view():
     for line in f.readlines():
       data = line.rstrip()
       user, passw = data.split('|')
-      print('User:', user, '| Password:', str(fer.decrypt(passw.encode())))
+      print('User:', user, '| Password:', fer.decrypt(passw.encode()))
+
+
 
 def add():
   name = input('Account Name: ')
   pwd = input('Password: ') 
 
   with open('passwords.txt', 'a') as f:
-    f.write(name + "|" + str(fer.encrypt(pwd.encode())) + '\n')
+    f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + '\n')
 
 while True:
   mode = input('Would you like to add a new password or view existing ones (view, add)?, press q to quit ').lower()
