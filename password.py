329dd9100c36082f7800947aaf737e6f279212d5
Tal <trimoni8@gmail.com>
2023-01-27 10:57:07 -0500

    key

--- password.py
+++ password.py
@@ -7,6 +7,8 @@ def write_key():
   with open('key.key', 'wb') as key_file:
     key_file.write(key)
 
+write_key()
+
 def view():
   with open('passwords.txt', 'r') as f:
     for line in f.readlines():
