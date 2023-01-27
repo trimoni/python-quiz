8f07a9d4ac2db91b46a4ac7e769d58363bb8cee7
Tal <trimoni8@gmail.com>
2023-01-27 10:58:37 -0500

    load key

--- password.py
+++ password.py
@@ -2,12 +2,18 @@ from cryptography.fernet import Fernet
 
 master_pwd = input('What is the master password? ')
 
-def write_key():
-  key = Fernet.generate_key()
-  with open('key.key', 'wb') as key_file:
-    key_file.write(key)
+# def write_key():
+#   key = Fernet.generate_key()
+#   with open('key.key', 'wb') as key_file:
+#     key_file.write(key)
 
-write_key()
+# write_key()
+
+def load_key():
+  file = open('key.key', 'rb')
+  key = file.read()
+  file.close()
+  return key
 
 def view():
   with open('passwords.txt', 'r') as f:
