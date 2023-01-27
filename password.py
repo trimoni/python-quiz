cbffa2423f26b1f25b5de5d814a6e14c646e6f53
Tal <trimoni8@gmail.com>
2023-01-27 10:37:57 -0500

    if else

--- password.py
+++ password.py
@@ -1,2 +1,9 @@
 pwd = input('What is the master password? ')
 
+mode = input('Would you like to add a new password or view existing ones (view, add)? ')
+if mode == 'view':
+  pass
+elif mode == 'add':
+  pass
+else:
+  print('Invalid mode.')
