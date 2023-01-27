e2c98303b82cae16e33ce8de6fb531fee5308551
Tal <trimoni8@gmail.com>
2023-01-27 10:45:00 -0500

    f.write

--- password.py
+++ password.py
@@ -9,6 +9,9 @@ def add():
   name = input('Account Name: ')
   pwd = input('Password: ') 
 
+  with open('passwords.txt', 'a') as f:
+    f.write(name + "|" + pwd)
+
 while True:
   mode = input('Would you like to add a new password or view existing ones (view, add)?, press q to quit ').lower()
   if mode == 'q':
