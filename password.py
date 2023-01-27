f96072f3e247e68b55e6fd89faa26201c01ce288
Tal <trimoni8@gmail.com>
2023-01-27 10:45:33 -0500

    \n line break

--- password.py
+++ password.py
@@ -10,7 +10,7 @@ def add():
   pwd = input('Password: ') 
 
   with open('passwords.txt', 'a') as f:
-    f.write(name + "|" + pwd)
+    f.write(name + "|" + pwd "\n")
 
 while True:
   mode = input('Would you like to add a new password or view existing ones (view, add)?, press q to quit ').lower()
